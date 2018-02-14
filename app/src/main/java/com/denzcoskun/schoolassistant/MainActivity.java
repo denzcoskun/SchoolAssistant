b3eaab4c333df14696fd402514bc63b19966a8b6
Deniz Coskun <denzcoskun@hotmail.com>
2018-02-15 01:09:47 +0300

    Project updated.

--- app/src/main/java/com/denzcoskun/schoolassistant/MainActivity.java
+++ app/src/main/java/com/denzcoskun/schoolassistant/MainActivity.java
@@ -8,12 +8,13 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
 import com.denzcoskun.schoolassistant.adapters.TabsPagerAdapter;
+import com.denzcoskun.schoolassistant.helpers.DataHelper;
+import com.denzcoskun.schoolassistant.models.MainModel;
 import com.denzcoskun.schoolassistant.weeklyschedule.activities.AddLessonActivity;
 import com.denzcoskun.schoolassistant.weeklyschedule.adapters.LessonAdapter;
 import com.denzcoskun.schoolassistant.weeklyschedule.models.DayModel;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -30,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
     FloatingActionButton floatingActionButton;
 
     private TabsPagerAdapter tabsPagerAdapter = null;
-    public static List<DayModel> dayModels;
+    public static MainModel mainModel = new MainModel();
     public static LessonAdapter[] lessonAdapters = new LessonAdapter[7];
 
     @Override
@@ -39,9 +40,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         getSupportActionBar().setElevation(0);
-        initDays();
+        DataHelper dataHelper = new DataHelper(MainActivity.this);
 
-        tabsPagerAdapter = new TabsPagerAdapter(getSupportFragmentManager(), getBaseContext(), dayModels);
+        if ((MainModel) dataHelper.getModel() == null) {
+            initDays();
+        }else {
+            mainModel = (MainModel) dataHelper.getModel();
+        }
+        tabsPagerAdapter = new TabsPagerAdapter(getSupportFragmentManager(), getBaseContext(), mainModel.dayModels);
 
         for (int i = 0; i < 7; i++) {
             tabLayout.addTab(tabLayout.newTab());
@@ -50,21 +56,26 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(tabsPagerAdapter);
         tabLayout.setupWithViewPager(viewPager);
 
-        floatingActionButton.setOnClickListener(v ->{
+        floatingActionButton.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, AddLessonActivity.class);
-            intent.putExtra("position",tabLayout.getSelectedTabPosition());
+            intent.putExtra("position", tabLayout.getSelectedTabPosition());
             startActivity(intent);
         });
     }
 
     public void initDays() {
-        dayModels = new ArrayList<>();
-        dayModels.add(new DayModel(getString(R.string.monday)));
-        dayModels.add(new DayModel(getString(R.string.tuesday)));
-        dayModels.add(new DayModel(getString(R.string.wednesday)));
-        dayModels.add(new DayModel(getString(R.string.thursday)));
-        dayModels.add(new DayModel(getString(R.string.friday)));
-        dayModels.add(new DayModel(getString(R.string.saturday)));
-        dayModels.add(new DayModel(getString(R.string.sunday)));
+        mainModel.dayModels = new ArrayList<>();
+        mainModel.dayModels.add(new DayModel(getString(R.string.monday)));
+        mainModel.dayModels.add(new DayModel(getString(R.string.tuesday)));
+        mainModel.dayModels.add(new DayModel(getString(R.string.wednesday)));
+        mainModel.dayModels.add(new DayModel(getString(R.string.thursday)));
+        mainModel.dayModels.add(new DayModel(getString(R.string.friday)));
+        mainModel.dayModels.add(new DayModel(getString(R.string.saturday)));
+        mainModel.dayModels.add(new DayModel(getString(R.string.sunday)));
+
+        mainModel.lessonsNames = new ArrayList<>();
+        mainModel.lessonsNames.add(getString(R.string.science));
+        mainModel.lessonsNames.add(getString(R.string.mathematics));
+        mainModel.lessonsNames.add(getString(R.string.history));
     }
 }
