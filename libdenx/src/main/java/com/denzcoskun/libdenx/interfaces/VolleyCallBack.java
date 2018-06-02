6864bf5b701049d0c1972532cddde9b7eb0059d3
Deniz Coskun <denzcoskun@hotmail.com>
2018-06-02 17:53:26 +0300

    libdenx reorganized.

--- libdenx/src/main/java/com/denzcoskun/libdenx/interfaces/VolleyCallBack.java
+++
@@ -1,8 +0,0 @@
-package com.denzcoskun.libdenx.interfaces;
-
-/**
- * Created by Denx on 1.06.2018.
- */
-public interface VolleyCallBack<T> {
-    public void OnSuccess(T result);
-}
--- libdenx/src/main/java/com/denzcoskun/libdenx/models/BaseResponseModel.java
+++
@@ -1,7 +0,0 @@
-package com.denzcoskun.libdenx.models;
-
-/**
- * Created by Denx on 1.06.2018.
- */
-public class BaseResponseModel {
-}
